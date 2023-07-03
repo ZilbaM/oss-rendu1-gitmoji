d8c38287509ba9a5d2e1e8c8abba795fa549f35b
Basile Maille <basmaille59@gmail.com>
2023-07-03 15:34:02 +0200

    feat:[setupFetch] Write fetch and add future crawling deps

---
+++ src/ApiService.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zilbam\OssRendu1Gitmoji;
+
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class ApiService
+{
+    private \Symfony\Contracts\HttpClient\HttpClientInterface $client;
+    private string $url;
+    public function __construct($url)
+    {
+        $this->client = HttpClient::create();
+        $this->url = $url;
+    }
+
+    /**
+     * @param string $path
+     * @return string|\Exception or Exception
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function get(string $path = "") : string | \Exception
+    {
+        try{
+            $response = $this->client->request('GET', $this->url . $path);
+            return $response->getContent();
+        } catch (\Exception $e) {
+            return $e;
+        }
+    }
+}
